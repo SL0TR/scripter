4d44c2a76cf6d8a5db77cad214bb253b96451782
SL0TR <mohaiminislam94@gmail.com>
2021-05-30 01:31:13 +0600

    chore: comment new algorithm update

--- src/helper.js
+++ src/helper.js
@@ -47,3 +47,48 @@ export function processTextContent({ textContent, characterList, isWin }) {
 
   return script;
 }
+
+// const initObj = {
+//   dialouge: "",
+//   characterName: null,
+// };
+// export function processTextContent({ textContent, characterList, isWin }) {
+//   const newLine = isWin ? "\r\n" : "\n";
+//   const textArray = textContent.split(newLine);
+
+//   console.log(textArray);
+
+//   let script = [];
+
+//   let scriptObj = { ...initObj };
+
+//   for (let i = 0; i <= textArray.length; i++) {
+//     const charString = textArray[i];
+
+//     // eslint-disable-next-line no-loop-func
+//     // characterList.forEach((el) => {
+//     //   if (charString && charString.toLowerCase() === el.toLowerCase()) {
+//     //     scriptObj.dialouge = el;
+//     //   }
+//     // });
+
+//     const isCharacter =
+//       charString &&
+//       characterList.some((el) => charString.toLowerCase() === el.toLowerCase());
+
+//     if (isCharacter) {
+//       scriptObj.characterName = charString;
+//     }
+
+//     if (!isCharacter) {
+//       scriptObj.dialouge += charString;
+//     }
+
+//     if (scriptObj.dialouge.lengtth > 0 && scriptObj.characterName) {
+//       script = [...script, scriptObj];
+//       scriptObj = { ...initObj };
+//     }
+//   }
+
+//   return script;
+// }
